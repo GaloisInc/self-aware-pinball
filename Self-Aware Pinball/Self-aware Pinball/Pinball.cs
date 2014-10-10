0306abc381021fb1e5e333f2935718250d13ae0f
Joseph Kiniry <kiniry@galois.com>
2014-10-10 12:00:10 -0700

    Skeleton project for library.

---
+++ Self-Aware Pinball/Self-aware Pinball/Pinball.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Self_aware_Pinball
+{
+    public class Pinball
+    {
+    }
+}
