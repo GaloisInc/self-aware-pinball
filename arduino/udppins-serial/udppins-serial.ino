5d70ef2671fc338dd0f46e9c27ad73e7257e16bf
Daniel M. Zimmerman <dmz@galois.com>
2014-10-10 14:43:50 -0700

    Updated to fix merge conflicts.

--- arduino/udppins-serial/udppins-serial.ino
+++ arduino/udppins-serial/udppins-serial.ino
@@ -146,29 +146,18 @@ void loop(void) {
       char buffer[UDP_READ_BUFFER_SIZE];
       int n = udpServer.readData(buffer, UDP_READ_BUFFER_SIZE);  // n contains # of bytes read into buffer
 
-<<<<<<< HEAD
-      if (n >= 1) {
-        uint8_t state = buffer[0];
-        for (int i = 0; i < 4; i++) {
-          digitalWrite(FIRST_FLIPPER_PIN + i, state & (1 << i) ? HIGH : LOW);
-        }
-        if (oldstate != state) {
-          lcd.setBacklight(WHITE);
-          lcd.setBacklight(GREEN);
-=======
       if (n >= 2) {
 	// Message 1: 0xff then byte with new state
 	if ((byte) buffer[0] == 0xff) {
 	  byte state = buffer[1];
-          for (int i = 0; i < 4; i++) {
-            digitalWrite(6+i, state & (1 << i) ? HIGH : LOW);
+          for (int i = 0; i < NUM_FLIPPERS; i++) {
+            digitalWrite(FIRST_FLIPPER_PIN + i, state & (1 << i) ? HIGH : LOW);
           }
           if (oldstate != state) {
             lcd.setBacklight(WHITE);
             lcd.setBacklight(GREEN);
           }
           oldstate = state;
->>>>>>> d76048ccfef0a008abba96ebc5812f26186a28b9
         }
 	// Message 2: length then that many bytes with new lcd text
 	else {
